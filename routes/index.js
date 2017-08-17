b88cbb069b377926458c195f34c4dda911fa2627
Aaron Drobek <adrobek09@gmail.com>
2017-08-17 17:02:13 -0400

    deadline

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,78 @@
 const express = require("express");
 const router = express.Router();
 
-router.use("/public", express.static("public"));
+let user = {username: "doctorclaw"  ,password: "opensaysme"};
 
+function authenticate(req, res, next) {
+  if (req.session.token) {
+    res.redirect("/");
+  } else {
+    console.log("No token");
+    next();
+  }
+}
+// router.use("/public", express.static("public"));
 
+router.get("/", function(req, res, next) {
+  if (req.session.token) {
+    next();
+  } else {
+    res.redirect("/login")
+  }
+}, function(req, res) {
+  res.render("/", req.session.user);
+});
 
+router.get("/", authenticate, function(req, res){
+  res.render("home");
+})
 
+router.get("/login", function(req, res){
+  res.render ("login");
+})
 
+router.post("/", function(req, res) {
+  let obj = {
+    username: req.body.username,
+    password: req.body.password
+  };
+  if (obj.username == user.username && obj.password == user.password) {
+   req.session.user = obj;
+   req.session.token = "hjhhuioiejij3242324";
+   res.redirect("/login");
+ } else {
+   res.redirect("/");
+ }
+});
 
+router.post('/login', function(req, res){
 
+  req.checkBody("username", 'username must be 8 to 25 characters ').isByteLength({min:8, max:25});
+  req.checkBody("username", "username cannot be empty.").notEmpty();
+  req.checkBody("password", 'password minimum of 8 characters required').isByteLength({min:8, max:25});
+  req.checkBody("password", "password cannot be empty.").notEmpty();
 
+  let errors = req.getValidationResult();
+  let messages = [];
 
+  errors.then(function(result) {
+    result.array().forEach(function(error) {
+      messages.push(error.msg);
+    });
 
+    let obj = {
+      errors: messages,
+      username: req.body.username,
+      password: req.body.password,
+    }
 
+    if (req.body.username && req.body.password){
+    // res.send("this is the response");
+      res.render("home");
+    }else{
+      res.render("error", obj);
+    }
+});
 
 
 
--- server.js
+++ server.js
@@ -5,15 +5,25 @@ const bodyParser = require("body-parser");
 const routes = require("./routes/index");
 const expressValidator = require("express-validator");
 const morgan = require("morgan");
+const session = require("express-session");
+const parseurl = require("parseurl");
 const app = express();
 
 app.engine("mustache", mustacheExpress());
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "mustache");
+app.set("layout", "layout");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(expressValidator());
+
+ app.use(session({
+   secret: "goodforparty",
+   resave: false,
+   saveUninitialized: false
+ }));
+
 
 app.use(morgan("dev"));
 
@@ -21,5 +31,5 @@ app.use(routes);
 
 
 app.listen(3000, function() {
-  console.log("running");
+  console.log("running on localhost3000");
 })
